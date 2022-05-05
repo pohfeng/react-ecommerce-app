69e4ecce5438ce83a88149177ac3f7375e0fb0b5
pohfeng <pohfeng926@gmail.com>
2022-05-05 23:11:34 +0800

    initialize app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/" exact></Route>
+      <Route path="/shop"></Route>
+      <Route path="/product-detail/:id"></Route>
+      <Route path="/wish-list"></Route>
+      <Route path="/checkout"></Route>
+    </Switch>
   );
 }
 
